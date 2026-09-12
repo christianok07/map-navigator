b495d9b95c198237c04bee06319346f0ef33564a
Okafor Christian <christian.collegegoals@gmail.com>
2026-09-12 17:03:47 +0200

    Add Location model

---
+++ src/main/java/org/example/model/Location.java
@@ -0,0 +1,62 @@
+package org.example.model;
+
+public class Location {
+
+    private final String id;
+    private final String name;
+    private final double latitude;
+    private final double longitude;
+
+    public Location(String id, String name, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Location)) {
+            return false;
+        }
+
+        Location location = (Location) o;
+
+        return id.equals(location.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
